116d6a6512b84a8189b69cec0a80bc62d654d5cb
MUSKAN <muskanprajapati0609@gmail.com>
2024-04-08 23:48:18 +0530

    ass

--- Day9_ASS2/Src/main.c
+++ Day9_ASS2/Src/main.c
@@ -21,8 +21,10 @@
 #include "stm32f4xx.h"
 #include "system_stm32f4xx.h"
 
+#include "lis3dsh.h"
+#include "i2c_lcd.h"
+#include "led.h"
 
-#include "lcd.h"
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
@@ -30,13 +32,62 @@
 
 int main(void)
 {
+	int ret;
+	LIS_Data val;
 	SystemInit();
-	LcdInit();
-	LcdPuts(LCD_LINE1, "JAI SHREE RAM");
-	LcdPuts(LCD_LINE2, "AYODHYA");
+	LedInit(LED_GREEN);
+	LedInit(LED_ORANGE);
+	LedInit(LED_RED);
+	LedInit(LED_BLUE);
+	LIS_Init();
+	DelayMs(50);
 
-	LcdShift(16);
+	while(1)
+	{
+		ret = LIS_DRdy();
+		if(ret)
+		{
+			val = LIS_GetData();
+			DelayMs(50);
 
+
+			if(val.x < -2000)
+			{
+				LedOn(LED_GREEN);
+				LedOff(LED_RED);
+				LedOff(LED_ORANGE);
+				LedOff(LED_BLUE);
+			}
+
+			else if(val.x > 2000)
+			{
+				LedOn(LED_RED);
+				LedOff(LED_GREEN);
+				LedOff(LED_ORANGE);
+				LedOff(LED_BLUE);
+			}
+
+			else if(val.y < -2000)
+			{
+				LedOn(LED_BLUE);
+				LedOff(LED_RED);
+				LedOff(LED_ORANGE);
+				LedOff(LED_GREEN);
+			}
+
+			else if(val.y > 2000)
+			{
+				LedOn(LED_ORANGE);
+				LedOff(LED_RED);
+				LedOff(LED_GREEN);
+				LedOff(LED_BLUE);
+			}
+		}
+	}
+		return 0;
 }
-	return 0;}
-}
+
+
+
+
+
